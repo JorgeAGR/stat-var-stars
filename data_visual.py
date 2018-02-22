f2044345111579e40f807c63c7c76de3b66cb328
Jorge A, Garcia <jorgeagr97@gmail.com>
2018-02-22 16:10:54 -0700

    Data Visualizer

--- data_visual.py
+++ data_visual.py
@@ -28,9 +28,9 @@ class PlotCanvas(tk.Frame):
         self.canvas.get_tk_widget().pack(side=tk.RIGHT,fill=tk.BOTH,expand=True)
         
         #Displays Matplotlib figure toolbar.
-        self.toolbar = NavigationToolbar2TkAgg(self.canvas, self)
-        self.toolbar.update()
-        self.canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        #self.toolbar = NavigationToolbar2TkAgg(self.canvas, self)
+        #self.toolbar.update()
+        #self.canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
 class Menu(tk.Frame):
     
@@ -52,13 +52,13 @@ class Menu(tk.Frame):
         frame_file_list.grid(row = 1, column = 0)
         
         self.scrollbar = tk.Scrollbar(frame_file_list)
-        self.scrollbar.grid(row = 0, column = 1)
+        self.scrollbar.grid(row = 0, column = 1, sticky = 'NS')
         
         self.list = tk.Listbox(frame_file_list, yscrollcommand = self.scrollbar.set, width = 16)
         for file in self.filelist:
             self.list.insert(tk.END, file)
         
-        self.list.grid(row = 0, column = 0)
+        self.list.grid(row = 0, column = 0, sticky = 'NS')
         self.scrollbar.config(command = self.list.yview)
         
     def readCampaigns(self):
@@ -85,12 +85,48 @@ class MainApp(tk.Tk):
         
         tk.Tk.wm_title(self, 'K2 Campaign Viewer')
         
-        self.Canvas = PlotCanvas(self)
-        self.Canvas.grid(row = 0, column = 0)
+        self.CanvasLC = PlotCanvas(self)
+        self.CanvasLC.grid(row = 0, column = 0, sticky = 'NEWS')
+        
+        self.CanvasA_LS = PlotCanvas(self)
+        self.CanvasA_LS.grid(row = 1, column = 0, sticky = 'NEWS')
         
         self.Menu = Menu(self)
-        self.Menu.grid(row = 0, column = 1, sticky = tk.N+tk.S)
+        self.Menu.grid(row = 0, column = 1, sticky = 'NS')
         
+        self.columnconfigure(0, weight = 10)
+        self.rowconfigure(0, weight = 10)
+        self.rowconfigure(1, weight = 10)
+        
+        self.Menu.list.bind('<<ListboxSelect>>', self.selectFile)
+    
+    def selectFile(self,event):
+        self.plot()
+    
+    def plot(self):
+        
+        # Maybe use Object class to manage all this file info
+        directory = 'k2c'+ self.Menu.campaign_dic[self.Menu.campaign_str.get()] + '/data/' + self.Menu.list.get(tk.ACTIVE)
+        with fits.open(directory) as hdul:
+            time = hdul[1].data['TIME']
+            lc = hdul[1].data['LC']
+            freq = hdul[2].data['FREQS']
+            A_LS = hdul[2].data['AMP_LOMBSCARG']
+        
+        self.CanvasLC.ax.clear()
+        plt.figure(1)
+        #self.CanvasLC.ax.
+        plt.plot(time, lc)
+        #self.CanvasLC.ax.set_xticks(np.arange(min(time), max(time)+1,10000))
+        #self.CanvasLC.ax.set_title('Object ID: ' + str(self.id) + '   Campaign: ' + str(self.campaign))
+        
+        self.CanvasA_LS.ax.clear()
+        plt.figure(2)
+        #self.CanvasA_LS.ax.
+        plt.plot(freq, A_LS)
+        
+        self.CanvasLC.f.canvas.draw()
+        self.CanvasA_LS.f.canvas.draw()
 
 app = MainApp()
 app.mainloop()        
